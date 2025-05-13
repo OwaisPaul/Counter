c085cb32f50cc25b3b5d0fbf89c63acc20c97647
OwaisPaul <152506988+OwaisPaul@users.noreply.github.com>
2025-05-13 16:18:08 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ let count = 0;
 
 document.getElementById("decreaseBtn").onclick = function() {
   count -= 1;
-  document.getElementById("countLabel").innerHTML = count;  
+  document.getElementById("countLabel").innerText = count;  
 }
 
 document.getElementById("resetBtn").onclick = function() {
     count = 0;
-    document.getElementById("countLabel").innerHTML = count;
+    document.getElementById("countLabel").innerText = count;
 }
 
 document.getElementById("increaseBtn").onclick = function() {
    count += 1; 
-   document.getElementById("countLabel").innerHTML = count;
+   document.getElementById("countLabel").innerText = count;
 }
